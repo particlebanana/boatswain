d62023113276956f2b67f59b5b78a84725d4b09e
Mike McNeil <mike@balderdash.co>
2015-09-21 17:19:37 -0500

    Finish mechanizing the script, add some validation.

--- bin/close-some-stale-issues.js
+++ bin/close-some-stale-issues.js
@@ -2,17 +2,67 @@
 
 require('machine-as-script')({
 
+  // Use the default environment variable namespace, 3 underscores ("___")
+  // envVarNamespace: '___',
+
   machine: {
 
 
     friendlyName: 'Close some stale issues',
 
 
-    description: '',
+    description: 'Close some stale issues in the specified GitHub repos.',
 
 
     inputs: {
 
+      repos: {
+        description: 'A set of repos that will be processed.',
+        example: [{ owner: 'balderdashy', repoName: 'sails' }],
+        required: true
+      },
+
+      credentials: {
+        description: 'The credentials that the bot will use to make authenticated requests to the GitHub API. This determines "who" the bot appears to be.',
+        extendedDescription: 'Either `accessToken`, `clientId`+`clientSecret`, or `username`+`password` keys may be provided.',
+        example: {},
+        required: true
+      },
+
+      maxNumIssuesToClosePerRepo: {
+        description: 'The maximum number of issues to close in any one repo as a result of running this script.',
+        extendedDescription: 'Currently, this must be <= 100 (because the page size of issue search results from the GitHub API is 100, and we don\'t currently handle multiple pages of results in this module)',
+        example: 1,
+        defaultsTo: 1
+      },
+
+      commentTemplate: {
+        description: 'The string template for the comment that will be posted when an issue is closed.',
+        extendedDescription: 'This supports lodash template notation with a handful of locals.  `issue` is the raw issue dictionary from the GitHub API and `repo` is one of the dictionaries provided to this script (i.e. with `repo.repoName` and `repo.owner`)',
+        example: 'A comment written in _GitHub-flavored markdown syntax_ and optionally taking advantage of <%- lodash.template.notation %>.',
+        defaultsTo:
+        'Thanks for posting, @<%- issue.user.login %>.  I\'m an experimental issue cleanup bot-- nice to meet you!'+
+        '\n'+
+        '\n'+
+        'It has been a couple of months since there have been any updates or new comments on this page.  If this issue has been resolved, please feel free to disregard the rest of this message.  '+
+        'On the other hand, if you are still waiting on a patch, please:\n\n'+
+        '  + review our [contribution guide](https://github.com/<%-repo.owner%>/<%-repo.repoName%>/blob/master/CONTRIBUTING.md) to make sure this submission meets our criteria (only _verified bugs_ with documented features, please;  no questions, commentary, or bug reports about undocumented features or unofficial plugins)'+
+        '\n'+
+        '  + create a new issue with the latest information, including updated version details with error messages, failing tests, etc.  Please include a link back to this page for reference.'+
+        '\n'+
+        '  + add a comment to _this_ issue with a link to the new issue (for people arriving from search engines)'+
+        '\n\n'+
+        'Thanks so much for your help!\n\n'+
+        '<3\n'+
+        '<%- repo.repoName %>-bot'
+      },
+
+      shelfLifeInDays: {
+        friendlyName: 'Shelf life',
+        description: 'How many days of inactivity to allow before an issue is considered stale.',
+        example: 60,
+        defaultsTo: 60
+      }
 
     },
 
@@ -22,172 +72,114 @@ require('machine-as-script')({
       var async = require('async');
       var Github = require('machinepack-github');
 
-      return exits.success();
-    }
+      // Validate shelfLifeInDays (ensure it is a positive integer)
+      if (inputs.shelfLifeInDays !== Math.floor(inputs.shelfLifeInDays)) {
+        return exits.error('Sorry, the GitHub Issue Search API does not support partial days. `shelfLifeInDays` must be a positive integer.');
+      }
+      else if (inputs.shelfLifeInDays < 1) {
+        if (inputs.shelfLifeInDays === 0) {
+          // what are you, some kind of open-source masochist?
+          return exits.error('What?!  I can\'t just close _all_ the issues. `shelfLifeInDays` must be a positive integer.');
+        }
+        if (inputs.shelfLifeInDays < 0) {
+          return exits.error('Can\'t read the future, sorry. `shelfLifeInDays` must be a positive integer.');
+        }
+      }
 
 
-  },
+      // Determine the event horizon for issue staleness (a JS timestamp)
+      var lastUpdatedBefore = (new Date()).getTime() - inputs.shelfLifeInDays*86400000;
+      console.log('Cleaning up issues that haven\'t seen an update in %d days.',inputs.shelfLifeInDays);
 
-  envVarNamespace: 'add_numbers__'
+      // For each repo...
+      async.each(inputs.repos, function (repo, next){
 
-}).exec({
-  success: function (sum){
-
-
-    console.log('Got result:', sum);
-  }
-});
-
-
-return;
-
-
-// The maximum number of issues to close in any one repo as a result
-// of running this script one time. Currently, this must be <= 100
-// (because the page size of issue search results from the GitHub API is 100,
-//  and we don't currently handle multiple pages of results in this module)
-//
-// Defaults to 3.
-var MAX_NUM_ISSUES_TO_CLOSE_PER_REPO = rttc.parseHuman(
-  process.env.MAX_NUM_ISSUES_TO_CLOSE_PER_REPO || '3',
-  'number'
-);
-
-
-
-// A set of repos that will be processed.
-var REPOS = rttc.parseHuman(
-  process.env.repos || [],
-  [
-    {
-      owner: 'string',
-      repoName: 'string'
-    }
-  ]
-);
-
-
-// The string template for the comment that will be posted by this bot
-// as the issue is closed.  It supports lodash template notation, and is
-// provided with the raw issue dictionary from the GitHub API as `issue` on
-// its scope, as well as repo info provided as `repo`.
-//
-// By default, this is a reasonable message.
-var COMMENT_TEMPLATE = rttc.validate('string',
-  process.env.COMMENT_TEMPLATE ||
-  'Thanks for posting, @<%- issue.user.login %>.  I\'m an experimental issue cleanup bot-- nice to meet you!'+
-  '\n'+
-  '\n'+
-  'It has been a couple of months since there have been any updates or new comments on this page.  If this issue has been resolved, please feel free to disregard the rest of this message.  '+
-  'On the other hand, if you are still waiting on a patch, please:\n\n'+
-  '  + review our [contribution guide](https://github.com/<%-repo.owner%>/<%-repo.repoName%>/blob/master/CONTRIBUTING.md) to make sure this submission meets our criteria (only _verified bugs_ with documented features, please;  no questions, commentary, or bug reports about undocumented features or unofficial plugins)'+
-  '\n'+
-  '  + create a new issue with the latest information, including updated version details with error messages, failing tests, etc.  Please include a link back to this page for reference.'+
-  '\n'+
-  '  + add a comment to _this_ issue with a link to the new issue (for people arriving from search engines)'+
-  '\n\n'+
-  'Thanks so much for your help!\n\n'+
-  '<3\n'+
-  '<%- repo.repoName %>-bot'
-);
-// Note that we convert it into a precompiled function using `_.template()`.
-COMMENT_TEMPLATE = _.template(COMMENT_TEMPLATE);
-
-
-// The credentials that the bot will use to make authenticated requests
-// to the GitHub API.  Either `accessToken`, `clientId`+`clientSecret`,
-// or `username`+`password` keys may be provided.
-// (required)
-var CREDENTIALS = rttc.parseHuman(CREDENTIALS, {});
-
-console.log(CREDENTIALS);
-return;
-
-
-
-// Construct a JS timestamp that represents two months ago today.
-var twoMonthsAgo = (new Date()).getTime() - 5184000000;
-
-// hack:TODO: remove
-twoMonthsAgo = (new Date()).getTime()+ 100000000;
-
-// For each repo...
-async.each(REPOS, function (repo, next){
-
-  // Fetch up to `MAX_NUM_ISSUES_TO_CLOSE_PER_REPO` of the oldest open issues in the repo.
-  Github.searchIssues({
-    owner: repo.owner,
-    repo: repo.repoName,
-    state: 'open',
-    lastUpdatedBefore: twoMonthsAgo,
-    credentials: CREDENTIALS,
-  }).exec({
-    error: function (err) {
-      // If an error was encountered, keep going, but log it to the console.
-      console.error('ERROR: Failed to search issues in repo ("'+repo.repoName+'")\n',err);
-      return next();
-    },
-    success: function (oldIssues){
-      console.log('Located at least %d old, open issues...',oldIssues.length);
-
-      // Only use the first `MAX_NUM_ISSUES_TO_CLOSE_PER_REPO` issues
-      // (chop off any extras from the end of the array)
-      oldIssues = oldIssues.slice(0, MAX_NUM_ISSUES_TO_CLOSE_PER_REPO);
-
-      // For each old issue...
-      async.each(oldIssues, function (oldIssue, next){
-
-        // Post a comment on the issue explaining what's happening.
-        Github.commentOnIssue({
+        // Fetch up to `inputs.maxNumIssuesToClosePerRepo` of the oldest
+        // open issues in the repository.
+        Github.searchIssues({
           owner: repo.owner,
           repo: repo.repoName,
-          issueNumber: oldIssue.number,
-          comment: COMMENT_TEMPLATE({
-            repo: repo,
-            issue: oldIssue
-          }),
-          credentials: CREDENTIALS,
+          state: 'open',
+          lastUpdatedBefore: lastUpdatedBefore,
+          credentials: inputs.credentials,
         }).exec({
-          error: function (err){
+          error: function (err) {
             // If an error was encountered, keep going, but log it to the console.
-            console.error('ERROR: Failed to comment on issue #'+oldIssue.number+':\n',err);
+            console.error('ERROR: Failed to search issues in repo ("'+repo.repoName+'")\n',err);
             return next();
           },
-          success: function (newCommentId){
+          success: function (oldIssues){
+            console.log('Located at least %d old, open issues...',oldIssues.length);
 
-            // Now close the issue.
-            Github.closeIssue({
-              owner: repo.owner,
-              repo: repo.repoName,
-              issueNumber: oldIssue.number,
-              credentials: CREDENTIALS,
-            }).exec({
-              error: function (err){
-                // If an error was encountered, keep going, but log it to the console.
-                console.error('ERROR: Failed to close issue #'+oldIssue.number+':\n',err);
-                return next();
-              },
-              success: function (){
-                return next();
-              }
-            }); // </Github.closeIssue>
+            // Only use the first `inputs.maxNumIssuesToClosePerRepo` issues
+            // (chop off any extras from the end of the array)
+            oldIssues = oldIssues.slice(0, inputs.maxNumIssuesToClosePerRepo);
+            console.log('...and closing the %d oldest ones.',oldIssues.length);
+
+            // For each old issue...
+            async.each(oldIssues, function (oldIssue, next){
+
+              // Post a comment on the issue explaining what's happening.
+              Github.commentOnIssue({
+                owner: repo.owner,
+                repo: repo.repoName,
+                issueNumber: oldIssue.number,
+                comment: _.template(inputs.commentTemplate)({
+                  repo: repo,
+                  issue: oldIssue
+                }),
+                credentials: inputs.credentials,
+              }).exec({
+                error: function (err){
+                  // If an error was encountered, keep going, but log it to the console.
+                  console.error('ERROR: Failed to comment on issue #'+oldIssue.number+':\n',err);
+                  return next();
+                },
+                success: function (newCommentId){
+
+                  // Now close the issue.
+                  Github.closeIssue({
+                    owner: repo.owner,
+                    repo: repo.repoName,
+                    issueNumber: oldIssue.number,
+                    credentials: inputs.credentials,
+                  }).exec({
+                    error: function (err){
+                      // If an error was encountered, keep going, but log it to the console.
+                      console.error('ERROR: Failed to close issue #'+oldIssue.number+':\n',err);
+                      return next();
+                    },
+                    success: function (){
+                      return next();
+                    }
+                  }); // </Github.closeIssue>
+                }
+              });//</Github.commentOnIssue>
+
+            }, function afterwards(err){
+              // If a fatal error was encountered processing this repo, bail.
+              // Otherwise, keep going.
+              return next(err);
+            }); //</async.each>
           }
-        });//</Github.commentOnIssue>
+        }); // </Github.searchIssues>
 
-      }, function afterwards(err){
-        // If a fatal error was encountered processing this repo, bail.
-        // Otherwise, keep going.
-        return next(err);
+      }, function afterwards(err) {
+        if (err) {
+          return exits.error(err);
+        }
+        return exits.success();
       }); //</async.each>
+
     }
-  }); // </Github.searchIssues>
-
-}, function afterwards(err) {
-  if (err) {
-    console.error('Script failed w/ fatal error:', err);
-    return;
   }
+}).exec({
 
-  console.log('Done.');
-}); //</async.each>
+  error: function (err) {
+    console.error('Script failed w/ fatal error:', err);
+  },
 
+  success: function (){
+    console.log('Done.');
+  }
+});
