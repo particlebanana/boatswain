6e6b6b98d86cd8161c3203ba108eb0a994ddff19
Mike McNeil <mike@balderdash.co>
2015-09-21 23:21:49 -0500

    Procfile.

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: NODE_ENV=production node bin/close-some-stale-issues.js
---
+++ bin/boatswain.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+console.log('Boatswain');
+console.log('v'+require('../package.json').version);
